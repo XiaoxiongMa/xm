70575011c867c2d7ae44b0d101bb24db25d0f685
Xiaoxiong Ma <mas1887415157@gmail.com>
2019-08-29 15:42:01 -0600

    added unity

---
+++ project 1/Assets/scripts/startscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(message: "Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(message: "updating");
+    }
+}
