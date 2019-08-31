5615b132326d24cbc440d986dcec72ed58f970ea
xiaoxiong ma <mas1887415157@gmail.com>
2019-08-30 23:50:42 -0600

    unity added

--- project 1/Assets/scripts/startscript.cs
+++ project 1/Assets/scripts/startscript.cs
@@ -8,7 +8,12 @@ public class startscript : MonoBehaviour
     void Start()
     {
         print(message: "Hello world");
-    }
+        print(message:"i love this class");
+        print(message:"i love coding");
+        print(message:"and i love my professor");
+        print(message:" my professor is best");
+        print(message: "i believe i can be good on coding");
+    }   
 
     // Update is called once per frame
     void Update()
