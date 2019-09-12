ac88e248c01e1bff1e1d4a4040821d403f9364aa
xiaoxiong ma <mas1887415157@gmail.com>
2019-09-11 23:57:04 -0600

    added my scirpt

--- project 1/Assets/scripts/myscripts.cs
+++ project 1/Assets/scripts/myscripts.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class newscripts : MonoBehaviour
-{
+
+{    public int HP = 100;
+            private string_name= "new";
+            private float att = 1000;
     // Start is called before the first frame update
     void Start()
     {
         print(message:"hi");
 		print(message:"welcome to World");
-
-        
+       
+              
     }
 
     // Update is called once per frame
