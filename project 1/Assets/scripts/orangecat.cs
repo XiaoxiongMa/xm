45c92dce421deb281ca76a84cd713385ee4dd2a2
xiaoxiong ma <mas1887415157@gmail.com>
2019-09-14 22:32:42 -0600

    added orangecat script

---
+++ project 1/Assets/scripts/orangecat.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orangecat : MonoBehaviour
+{
+	public UnityEven doesStuff;
+
+    private void OnMouseDown()
+    {
+        doesStuff.Invoke();
+    }
+}
