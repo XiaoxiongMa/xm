690ecbc6c72b3aefcacd2e4634b5c8cb7c8380c0
xiaoxiong ma <mas1887415157@gmail.com>
2019-09-20 22:44:01 -0600

    added

--- project 1/Assets/scripts/myscripts.cs
+++ project 1/Assets/scripts/myscripts.cs
@@ -8,16 +8,19 @@ public class MyScripts : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       // print(message:"hi");
-	   // print(message:"welcome to World");
+        // print(message:"hi");
+	    // print(message:"welcome to World");
         // int a = 5;
         //int b = ++a;
-         //print(message:("b={0}", b));
+        //print(message:("b={0}", b));
 
         int a = 5;
         int b = a--;
-        print(message: ("b={0},a={1}", b,a);
-        
+        print(message: ("b={0},a={1}", b,a));
+
+        int a = 10;
+        int b = ++a;
+        print(message: ("b={0}", b));
     }
 
     // Update is called once per frame
