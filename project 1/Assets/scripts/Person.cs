ae19f07048c0fa814bde80926329b77ee28cac20
xiaoxiong ma <mas1887415157@gmail.com>
2019-09-25 23:58:14 -0600

    added person script

---
+++ project 1/Assets/scripts/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour 
+{
+    public int age;
+    public string _name;
+
+    public Person()
+    {
+        this.age = 20;
+        this._name = "human";
+        Debug.Log("i am a human");
+    }
+
+
+}
