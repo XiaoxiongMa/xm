32be60ae94d14d9d740f6c40c02e7934ff1f35d4
xiaoxiong ma <mas1887415157@gmail.com>
2019-09-27 23:53:31 -0600

    added playercontroll

---
+++ project 1/Assets/scripts/PlayerControll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         Person Pr;
+         GameObject.Find("Main Camera").GetComponent<Person>();
+                
+         Pr.age = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
