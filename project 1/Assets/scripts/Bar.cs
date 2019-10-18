59ea1170b74c82e866264b5a4522825f0580fc7f
xiaoxiong ma <mas1887415157@gmail.com>
2019-10-17 20:37:13 -0600

    added bar script

---
+++ project 1/Assets/scripts/Bar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour
+{
+    [Header("Graphic")]
+    public Image BarImg;
+    public transform particlesTransform;
+
+    [Header("Settings")]
+    public float Speed;
+
+    private bool isDone = false;
+    private particleSystem[] pSysems;
+
+    void Start()
+    {
+        BarImg.fillAmount = 0;
+        pSysems = particlesTransform.GetComponentsInChildren<particlesTransform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDone)
+            return;
+
+        BarImg.fillAmount += (Speed * Time,delataTime) / 100;
+
+        if (BarImg.fillAmount >= 1.0f)
+        {
+            isDone = true;
+            foreach (var particle in pSysems)
+                particle.Play();
+        }
+
+    }
+}
