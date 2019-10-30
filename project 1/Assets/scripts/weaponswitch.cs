0d87105dd5d74a46b557dd4ff752e5e9723ca7c2
xiaoxiong ma <mas1887415157@gmail.com>
2019-10-29 22:02:57 -0600

    added weaponswitch

---
+++ project 1/Assets/scripts/weaponswitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponswitch : MonoBehaviour
+{
+    
+    public weapon states
+    {
+        Start,
+        Playing,
+        End
+    }
+
+    public states currentStates;
+    public UnityEvent OnStartEvent, OnPlatingEvent, OnEndEvent;
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentStates)
+        {
+            case states.Start:
+                break;
+            case states.Playing:
+                break;
+            case states.End :
+                OnEndEvent.Invoke();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
