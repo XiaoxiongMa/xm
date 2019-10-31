dc10c5f1c631eca0f1977340ca3838fd026c4c0e
Xiaoxiong Ma <mas1887415157@gmail.com>
2019-10-31 15:48:53 -0600

    added AI

--- project 1/Assets/scripts/weaponswitch.cs
+++ project 1/Assets/scripts/weaponswitch.cs
@@ -2,33 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class weaponswitch : MonoBehaviour
-{
-    
-    public weapon states
-    {
-        Start,
-        Playing,
-        End
-    }
-
-    public states currentStates;
-    public UnityEvent OnStartEvent, OnPlatingEvent, OnEndEvent;
-    public WeaponData WeaponData;
-    // Update is called once per frame
-    void Update()
-    {
-        switch (currentStates)
-        {
-            case states.Start:
-                break;
-            case states.Playing:
-                break;
-            case states.End :
-                OnEndEvent.Invoke();
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-    }
-}
+//public class weaponswitch : MonoBehaviour
+//{
+//    
+//    public weapon states
+//    {
+//        Start,
+//        Playing,
+//        End
+//    }
+//
+//    public states currentStates;
+//    public UnityEvent OnStartEvent, OnPlatingEvent, OnEndEvent;
+//    public WeaponData WeaponData;
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        switch (currentStates)
+//        {
+//            case states.Start:
+//                break;
+//            case states.Playing:
+//                break;
+//            case states.End :
+//                OnEndEvent.Invoke();
+//                break;
+//            default:
+//                throw new ArgumentOutOfRangeException();
+//        }
+//    }
+//}
