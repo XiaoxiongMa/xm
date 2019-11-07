768573c8460b3bbf4b46dec5756037873c6ec7a5
xiaoxiong ma <mas1887415157@gmail.com>
2019-11-06 23:07:29 -0700

    added

---
+++ project 1/Assets/scripts/Configuring AI with Scriptable Objects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+
+public class ConfiguringAIwithScriptableObjects : scriptableobject
+{
+    public float InitialValue
+
+    [NonSerialized]
+    public float RuntimeValue;
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = InitialValue;
+    
+    } 
+
+    public void OnBeforeSerialize() { }
+}
