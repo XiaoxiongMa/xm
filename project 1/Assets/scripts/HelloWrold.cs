0b870badbadf9addee7bea0ca68f821418598a94
xiaoxiong ma <mas1887415157@gmail.com>
2019-11-07 23:43:31 -0700

    added helloworld

---
+++ project 1/Assets/scripts/HelloWrold.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HelloWorldLib;
+{
+    public class HelloWorld
+    {
+        public string SayHello(string who)
+        {
+            if (string.IsNullOrEmpty)
+            {
+                who = "world";
+            }
+        }
+    }
+}
