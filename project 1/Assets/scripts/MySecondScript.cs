f252c1fe3ffb17e63498c719c42c15bae98b27f4
xiaoxiong ma <mas1887415157@gmail.com>
2019-11-13 15:50:39 -0700

    added

--- project 1/Assets/scripts/MySecondScript.cs
+++ project 1/Assets/scripts/MySecondScript.cs
@@ -12,7 +12,7 @@ public class MySecondScript : MonoBehaviour
         {
             print(message:"Hello World!");
             print(message:"good!");
-            
+            print(message:"GO!");
         }
     }
 
