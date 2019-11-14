62a2a47740fee2cfeec24db528f5561258487ee6
Xiaoxiong Ma <mas1887415157@gmail.com>
2019-11-14 15:47:18 -0700

    added cortineclasss

---
+++ project 1/Assets/Scenes/Coroutineclass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Coroutineclass : MonoBehaviour
+{
+    public float seconds = 1f;
+    public IntData index;
+    public bool canRun = true;
+    public WaitForSeconds wfsObj;
+    
+    public void Run()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        StartCoroutine(routine:OnRun());
+    }
+     IEnumerator OnRun()
+    {
+        while (index.value > 0)
+        {
+            index.value--;
+            Debug.Log(index.value);
+            yield return wfsObj;
+        }
+    }
+}
