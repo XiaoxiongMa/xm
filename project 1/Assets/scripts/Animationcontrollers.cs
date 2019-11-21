89069e09b74d2a14037ced26f0aff34c0883a386
xiaoxiong ma <mas1887415157@gmail.com>
2019-11-20 23:49:31 -0700

    added

--- project 1/Assets/scripts/Animationcontrollers.cs
+++ project 1/Assets/scripts/Animationcontrollers.cs
@@ -7,7 +7,7 @@ public class Animationcontrollers : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(message:Animationcontrollers)
     }
 
     // Update is called once per frame
