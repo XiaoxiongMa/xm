cf77b3a7c099da36d217867d3e436effd15399f9
Xiaoxiong Ma <mas1887415157@gmail.com>
2019-11-21 15:53:21 -0700

    added gamestates

---
+++ project 1/Assets/GameActionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+    public GameAction GameActionObj;
+    public UnityEvent handlerEvent;
+
+    void Start()
+    {
+        GameActionObj.action = RaiseHandler;
+    }
+
+
+    public void RaiseHandler()
+    {
+        handlerEvent.Invoke();
+    }
+}
