77494c55184d3de17bd648dd35c9011742c2d1ed
xiaoxiong ma <mas1887415157@gmail.com>
2019-12-07 00:34:03 -0700

    ADDED

--- project 1/Assets/Scenes/FristClass.cs
+++ project 1/Assets/Scenes/FristClass.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 //    // Start is called before the first frame update
 //    void Start()
 //    {
-//        
+//        Debug.Log(message:"GO!!")
 //    }
 //
 //    // Update is called once per frame
