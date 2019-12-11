03fa0abca706c21760f50449629914057581678b
xiaoxiong ma <mas1887415157@gmail.com>
2019-12-10 20:07:23 -0700

    added

--- project 1/Assets/Scenes/FristClass.cs
+++ project 1/Assets/Scenes/FristClass.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 //public class FristClass : MonoBehaviour
 //{
 //    public UnityEvent UnityEvent;
+    public UnityEngine UnityEngine;
+    
+
 //    // Start is called before the first frame update
 //    void Start()
 //    {
